e516aea9f79e87973bee3fc82c51bca348bf62c4
Jonathan Respeto <jrespeto@akamai.com>
2023-07-05 16:56:51 -0400

    oops

--- opt/app/configs/config.py
+++ opt/app/configs/config.py
@@ -1,14 +1,14 @@
 
 # Bot token from discord developer portal (https://discord.com/developers/applications)
-TOKEN = 'OTc2MTA5MTIxODM1MjY2MDU4.G9C5CS.THSJLD1-9Nhyjxii7nYdbExV9-6Y_35N8_IdtA'
+TOKEN = 'TOKEN'
 
 # Bot prefix for commands
 PREFIX = '!'
 
 # Server ID for the bot to run on.
-GUILD_ID = 752345180463693895
+GUILD_ID = 000000000000000000
 
-ROLE_ID = 986010375466139679
+ROLE_ID = 000000000000000000
 # List of cogs to load on startup (cogs are located in opt/app/cogs)
 # Cogs are loaded in the order they are listed here
 MODS = [# 'BotHelp',
